bd417519a20f6fb73cce6183c72a1aee1d70de78
jeanbezet <jean.bezet@etu.umontpellier.fr>
2017-12-12 11:09:33 +0100

    clean messages from update() and corrects if tests

--- src/server/ObservableOriginatorServer.java
+++ src/server/ObservableOriginatorServer.java
@@ -47,7 +47,7 @@ public class ObservableOriginatorServer extends ObservableServer
    * @param client The connection to the client.
    */
   protected synchronized void handleMessageFromClient
-    (Object message, ConnectionToClient client)
+    (ConnectionToClient client, Object message)
   {
     setChanged();
     notifyObservers(new OriginatorMessage(client, message));
